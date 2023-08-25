dd908ce620c8e0a40d571226b23072ad4b6c2ee2
renMengbo <100649373+renMengbo@users.noreply.github.com>
2023-08-25 13:28:32 +0930

    Add files via upload

---
+++ Finder.cpp
@@ -0,0 +1,21 @@
+#include "Finder.h"
+
+    using namespace std;
+
+    vector<int> Finder::findSubstrings(string s1, string s2) {
+
+        vector<int> result;
+        int index =0;
+        for(size_t i = 1; i <= s2.size(); i++) {
+            size_t found = s1.find(s2.substr(0, i),index);
+            if (found != string::npos) {
+                result.push_back(found);
+                index =found;
+            } else {
+                result.push_back(-1);
+            }
+        }
+        return result;
+    }
+
+   
