a6812552791850e4e5b76b3cff1ed97908979a2d
renMengbo <100649373+renMengbo@users.noreply.github.com>
2023-08-25 14:03:04 +0930

    Update Finder.cpp

--- Finder.cpp
+++ Finder.cpp
@@ -5,7 +5,7 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
-        int index =0;
+        int index =1;
         for(size_t i = 1; i <= s2.size(); i++) {
             size_t found = s1.find(s2.substr(0, i),index);
             if (found != string::npos) {
