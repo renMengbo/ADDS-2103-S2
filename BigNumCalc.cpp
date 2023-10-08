022f7df8d09407e7be8462c92b42e90c8e88b4d8
renMengbo <100649373+renMengbo@users.noreply.github.com>
2023-10-08 23:10:14 +0800

    Add files via upload

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -2,6 +2,7 @@
 #include <iostream>  
 #include <list>  
 #include <string>
+# include <iostream>
   
 using namespace std;    
  
@@ -16,68 +17,113 @@ std::list<int> BigNumCalc::buildBigNum(std::string numString) {
 std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2) {  
     std::list<int> result;  
     int carry = 0;  
-      
-    for (int i = 0; i < max(num1.size(), num2.size()); i++) {  
-        int digit1 = i < num1.size() ? num1.back() : 0;  
-        int digit2 = i < num2.size() ? num2.back() : 0;  
-        int sum = digit1 + digit2 + carry;  
+
+    auto it1 = num1.rbegin();
+    auto it2 = num2.rbegin();
+
+    while (it1 != num1.rend() && it2 != num2.rend()) {
+//        cout <<"+: " << *it1 << "--" << *it2 <<endl;
+        int sum = *it1 + *it2 + carry;  
         carry = sum / 10;  
         result.push_back(sum % 10);  
-        num1.pop_back();  
-        num2.pop_back();  
-    }  
-      
+        ++it1;
+        ++it2;
+    }
+
+    // Add remaining elements from the longer list
+    while (it1 != num1.rend()) {
+//        cout <<"+: " << *it1 << "--" << "*it2" <<endl;
+        int sum = *it1 + carry;  
+        carry = sum / 10;  
+        result.push_back(sum % 10);  
+        ++it1;
+    }
+
+    while (it2 != num2.rend()) {
+//        cout <<"+: " << "*it1" << "--" << *it2 <<endl;
+        int sum = *it2 + carry;  
+        carry = sum / 10;  
+        result.push_back(sum % 10);  
+        ++it2;
+    }
+    
     if (carry > 0) {  
         result.push_back(carry);  
     }  
-      
-    return result;  
-}  
+
+    result.reverse();
+    return result;
+}
+
   
 std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {  
+    
     std::list<int> result;  
-    int borrow = 0;  
-      
-    for (int i = 0; i < num1.size(); i++) {  
-        int digit1 = num1.back();  
-        int digit2 = i < num2.size() ? num2.back() : 0;  
-        int difference = digit1 - digit2 - borrow;  
+
+     int borrow = 0;  
+   
+    auto it1 = num1.rbegin();
+    auto it2 = num2.rbegin();
+
+    while (it1 != num1.rend() && it2 != num2.rend()) {
+//       cout <<"+: " << *it1 << "--" << *it2 <<endl;
+        int difference = *it1 - *it2 - borrow;  
         if (difference < 0) {  
             difference += 10;  
             borrow = 1;  
         } else {  
             borrow = 0;  
         }  
-        result.push_back(difference);  
-        num1.pop_back();  
-        if (i < num2.size()) {  
-            num2.pop_back();  
+        result.push_back(difference); 
+        ++it1;
+        ++it2;
+    }
+
+    // Add remaining elements from the longer list
+    while (it1 != num1.rend()) {
+//        cout <<"+: " << *it1 << "--" << "*it2" <<endl;
+        int difference = *it1 - borrow;  
+        if (difference < 0) {  
+            difference += 10;  
+            borrow = 1;  
+        } else {  
+            borrow = 0;  
         }  
-    }  
+        result.push_back(difference); 
+        ++it1;
+    }
+
+    result.reverse();
       
     return result;  
 }  
   
 std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2) {  
+
     std::list<int> result;  
     int carry = 0;  
-      
-    for (int i = 0; i < num1.size(); i++) {  
-        int digit1 = num1.back();  
-        for (int j = 0; j < num2.size(); j++) {  
-            int digit2 = num2.back();  
-            int product = digit1 * digit2 + carry;  
-            carry = product / 10;  
-            result.push_back(product % 10);  
-        }  
-        num1.pop_back();  
-        num2.pop_back();  
-    }  
-      
-    while (carry > 0) {  
+
+    auto it1 = num1.rbegin();
+
+    while (it1 != num1.rend() ) {
+        auto it2 = num2.rbegin();
+        cout <<"*it1 : " << *it1 << endl;
+        while (it2 != num2.rend()) {
+        int product = (*it1) * (*it2) + carry; 
+        carry = product / 10;  
+        result.push_back(product % 10);  
+ //                       cout <<"*it2 : "  << *it2 << " product: --" << product<< " carry: --"<< carry << " push_back: --"<< product % 10 <<endl; 
+        ++it2;
+        }
+        ++it1;
+    }
+
+  
+    if (carry > 0) {  
         result.push_back(carry);  
-        carry /= 10;  
     }  
+
+    result.reverse();
       
-    return result;  
+    return result;      
 }
