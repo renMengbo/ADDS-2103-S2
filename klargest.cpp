10086f76a77739732f1460d3d870b2b0e6852ba5
renMengbo <100649373+renMengbo@users.noreply.github.com>
2024-05-16 22:46:31 +0800

    Update klargest.cpp

--- klargest.cpp
+++ klargest.cpp
@@ -11,7 +11,7 @@ int kth_largest(vector<int> values, int k)
     
     for (int value : values) {
         max_heap.push(value); // 将值添加到最大堆中
-        if (max_heap.size() > static cast<unsignedint>(k)) { // 如果堆的大小超过了k,则从堆中移除最小值并将其添加回原始向量中
+        if (max_heap.size() > static cast<unsigned int>(k)) { // 如果堆的大小超过了k,则从堆中移除最小值并将其添加回原始向量中
             max_heap.pop();
             values.push_back(max_heap.top());
         }
